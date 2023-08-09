9e6964fd497aa35e46d391bce5bbd2ea902079bf
laurence bartrop-clist <laurence.bartrop-clist@cardstream.com>
2023-08-09 15:56:14 +0000

    Initial commit of PrestaShop module

---
+++ paymentnetwork/config.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+	'gateway_title' => 'Payment Network Gateway',
+	'method_description' => 'Pay securely via Credit / Debit Card with PaymentNetwork',
+	'admin_module_settings_title' => 'Payment Network Settings',
+	'author' => 'PaymentNetwork',
+	'default_merchant_id' => 'merchant_id_here',
+	'default_secret' => 'merchant_secret_here',
+	'redirect_notice' => 'You will be redirected to the Payment Gateway for payment',
+	'formfill_notice' => 'Please fill in your payment details',
+	'logo' => '/logo.gif',
+];
